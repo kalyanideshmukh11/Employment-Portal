8fc39bbc7d11a81915a25e2119dd6716b3ab9709
Navya Chennagiri Ravikumar <navya.chennagiriravikumar@sjsu.edu>
2020-11-26 00:19:29 -0800

    Search by company backend

--- kafka-backend/services/search_topic.js
+++ kafka-backend/services/search_topic.js
@@ -1,9 +1,9 @@
-const review = require('../models/review');
+const review = require("../models/review");
 
-module.exports.reviewService = function (msg, callback) {
-  console.log('In review service path', msg.path);
+module.exports.searchService = function (msg, callback) {
+  console.log("In review service path", msg.path);
   switch (msg.path) {
-    case 'searchByCompany':
+    case "searchByCompany":
       searchByCompany(msg, callback);
       break;
   }
@@ -12,17 +12,32 @@ module.exports.reviewService = function (msg, callback) {
 async function searchByCompany(msg, callback) {
   let err = {};
   let response = {};
-  console.log('In search by company details topic service. Msg: ', msg);
+  console.log("In search by company details topic service. Msg: ");
   console.log(Object.keys(msg.body));
-  let ids = Object.keys(msg.body)
-  await review.find(msg.body)
-    .then((data) => {
-      response.status = 200;
-      response.message = 'Inserted Successfully';
-      response.data = data;
-      return callback(null, response);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  let ids = Object.keys(msg.body);
+  await review.aggregate(
+    [
+      {
+        $match: { sql_company_id: { $in: ids } },
+      },
+      {
+        $group: {
+          _id: "$sql_company_id",
+          reviews: { $sum: 1 },
+        },
+      },
+    ],
+    function (err, results) {
+      console.log("Results:", results);
+      for (each of results) {
+        msg.body[each._id].reviews = each.reviews;
+      }
+    }
+  );
+
+  callback(null, msg.body);
+  // .find({ sql_company_id: { $in: ids } }, function (err, result) {
+  //   console.log("reviews list:", result);
+  //   callback(null, result);
+  // });
 }
